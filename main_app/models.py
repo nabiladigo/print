99c407b305e719ce5384e87d21af0ab25a6b1d56
nabiladigo <nabiladigo91@gmail.com>
2022-02-09 18:23:49 -0500

    create views and models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Print(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.IntegerField(default=0)
+    price = models.TextField(max_length=500)
+    verified_artist = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.views import View 
+from .models import Print, Card
 
-# Create your views here.
+
+
+class Home(TemplateView):
+    template_name = "home.html"
+
+class About(TemplateView):
+    template_name = "about.html"
+
+class PrintList(TemplateView):
+    template_name = "print_list.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name")
+        if name != None:
+            context["prints"] = Print.objects.filter(name__icontains=name)
+            context["header"] = f"Searching for {name}"
+        else:
+            context["prints"] = Print.objects.all()
+            context["header"] = "Trending Artists"
+        return context
+
+class PrintCreate(CreateView):
+    model = Print
+    fields = ['name', 'image', 'price']
+    template_name = "artist_create.html"
+    def get_success_url(self):
+        return reverse('artist_detail', kwargs={'pk': self.object.pk})
+
+class PrintDetail(DetailView):
+    model = Print
+    template_name = "print_detail.html"
+
+class PrintUpdate(UpdateView):
+    model = Print
+    fields = ['name', 'img', 'price']
+    template_name = "print_update.html"
+    def get_success_url(self):
+        return reverse('print_detail', kwargs={'pk': self.object.pk})
+
+class PrintDelete(DeleteView):
+    model = Print
+    template_name = "print_delete_confirmation.html"
+    success_url = "/prints/"
+
