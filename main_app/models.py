58478f59bd5b073ffe0c4186422d13a7c4a11923
nabiladigo <nabiladigo91@gmail.com>
2022-02-09 19:47:46 -0500

    create templates

--- main_app/models.py
+++ main_app/models.py
@@ -4,9 +4,9 @@ from django.db import models
 class Print(models.Model):
     
     name = models.CharField(max_length=100)
-    image = models.IntegerField(default=0)
+    image = models.CharField(max_length=250)
     price = models.TextField(max_length=500)
-    verified_artist = models.BooleanField(default=False)
+    verified_print = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
