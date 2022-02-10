ef271c92373b05aa29e1dcdfff8518fc83bccbc1
nabiladigo <nabiladigo91@gmail.com>
2022-02-09 22:53:07 -0500

    create mugs, puzzles, photo books files

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Print, Card
+from .models import Print, Card, Mug, Photo, Puzzle
 
 
 admin.site.register(Print)
 admin.site.register(Card)
+admin.site.register(Mug)
+admin.site.register(Photo)
+admin.site.register(Puzzle)
